4121abe951987a8607e9cb9c71d1e44b3735057d
Mahandrumahesh <Mahandrumahesh@users.noreply.github.com>
2017-04-03 22:34:18 -0400

    git hub auth added

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var Account = require('../models/account');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'COMP2068 - Players',
+        title: 'Players',
         user: req.user
     });
 });
@@ -91,5 +91,18 @@ router.get( '/google/callback',
         successRedirect: '/players',
         failureRedirect: '/login'
     }));
+//github
+router.get('/github', passport.authenticate('github', { scope: 'email'}));
 
+router.get('/github/callback',
+    passport.authenticate('github',
+        {
+            failureRedirect: '/login',
+            scope: 'email'
+
+        }),
+    function(req, res) {
+        // Successful authentication, redirect to lists.
+        res.redirect('/players');
+    });
 module.exports = router;
