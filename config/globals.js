fd015fe0832ca75c04ad4cd1706449fd561e6127
Mahandrumahesh <Mahandrumahesh@users.noreply.github.com>
2017-04-06 16:13:56 -0400

    Update globals.js

--- config/globals.js
+++ config/globals.js
@@ -7,12 +7,12 @@ module.exports = {
     facebook: {
         clientID: '1944680639084904',
         clientSecret: '8690b13596b9fa684eac11a6d33beda4',
-        callbackURL: 'http://localhost:3000/facebook/callback'
+        callbackURL: 'https://mahesh-players.herokuapp.com/facebook/callback'
     },
+    //authentication for the google
     google: {
         clientID: '865293236726-uaub0ga32tbmqk2ccju6ll1dp78k5uoq.apps.googleusercontent.com',
         clientSecret: 'pwhFjTVglYTu6WjElVR9WrzY',
         callbackURL: 'http://localhost:3000/google/callback'
     }
-    
 };
