ab6b5b711820bb1eddb4f4d949465056c6beb2ed
Mahandrumahesh <Mahandrumahesh@users.noreply.github.com>
2017-04-06 18:23:10 -0400

    added file upload method

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var formidable = require('formidable');
+var fs = require('fs');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -94,6 +96,38 @@ passport.deserializeUser(Account.deserializeUser());
 app.use('/', index);
 app.use('/users', users);
 app.use('/players', players); // handle all requests at /books with books router
+
+app.post('/upload', function(req, res){
+
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
